d1530214bf43904b6bdd5101681032f001d95227
Evan Bangun <evan.bangun.eb@gmail.com>
2018-10-26 11:18:43 +0700

    mudah mudahan ga ada bug

--- application/models/m_user.php
+++ application/models/m_user.php
@@ -4,7 +4,7 @@ class m_user extends CI_Model
 {
 	public function tampilassignment($id_user)
 	{
-		$query = $this->db->query("	SELECT md.ms_num, md.ac_type, md.task_code, md.rvcd, md.resp,
+		$query = $this->db->query("	SELECT eta.ms_num, eta.ac_type, md.task_code, md.rvcd, md.resp,
 											SUBSTRING_INDEX(GROUP_CONCAT(CAST(status AS CHAR) ORDER BY etp.create_date DESC),',',1) AS status,
 											concat(md.task_desc,'<br><br>', md.task_subdesc) as descr,
 											group_concat(DISTINCT concat('>>', ms.sg_code,' ', ms.sg_num) SEPARATOR '<br>') as camp_sg,
@@ -17,7 +17,8 @@ class m_user extends CI_Model
 									Left Join msi_ref mr ON mr.ms_num = eta.ms_num AND mr.ac_type = eta.ac_type
 									Left Join ev_task_process etp ON etp.ms_num = eta.ms_num AND etp.ac_type = eta.ac_type 
 									WHERE eta.id_user = '$id_user'
-									Order By md.ms_num ASC
+									Group By eta.ms_num, eta.ac_type
+									Order By eta.ms_num ASC
 									LIMIT 100");
 
   		return $query->result_array();
